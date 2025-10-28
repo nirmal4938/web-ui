e739b0edf989750d88df8736c9637102189afc02
nirmalkj2025 <nirmal@kayjayglobal.com>
2025-10-28 09:51:16 +0530

    fix added production api url in axios

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // ✅ Base API instance
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:5000/api", // Adjust based on backend
+  baseURL: import.meta.env.VITE_API_PROD_URL || "http://localhost:5000/api", // Adjust based on backend
   headers: {
     "Content-Type": "application/json",
   },
--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,7 +1,7 @@
 // src/services/axios.ts
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_PROD_URL;
 
 const api = axios.create({
   baseURL,
