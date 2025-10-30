1b51b8eb4d4e2b7d3e7ea5e9e53180d63b3a1029
nirmalkj2025 <nirmal@kayjayglobal.com>
2025-10-30 18:24:55 +0530

    30-10-2925'1

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:  "http://localhost:5000/api",
+  baseURL:  import.meta.env.VITE_API_PROD_URL || "http://localhost:5000/api",
   headers: { "Content-Type": "application/json" },
 });
 
